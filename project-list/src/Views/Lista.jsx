74bd044ef37e1e45294fcd1096d587ce801c35e3
wedjat11 <jamontiel@ammper.com>
2024-07-07 16:42:52 -0600

    titulo añadido

--- project-list/src/Views/Lista.jsx
+++ project-list/src/Views/Lista.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import ListContainer from '../Components/ListContainer'
+import Typography from '@mui/material/Typography'
 
 export default function MainList(){
     return(
+        <>
+        <Typography 
+        variant="h3" 
+        color="initial" 
+        align= 'center'
+        sx={{padding: 4}}>To do</Typography>
         <ListContainer/>
+        </>
+        
     )
 }
