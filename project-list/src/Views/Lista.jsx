25dd1496eb3b36f7a550e5972ada4b53067c9977
wedjat11 <jamontiel@ammper.com>
2024-07-07 16:38:29 -0600

    Creación de tabla

---
+++ project-list/src/Views/Lista.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react'
+import ListContainer from '../Components/ListContainer'
+
+export default function MainList(){
+    return(
+        <ListContainer/>
+    )
+}
